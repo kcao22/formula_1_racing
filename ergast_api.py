b61b93a8b85a04e5f45cc2f1d01b14b708004085
kcao22 <kevincao222@gmail.com>
2023-12-09 20:50:36 -0800

    created api wrapper classes. working through logging

---
+++ ergast_api.py
@@ -0,0 +1,40 @@
+# Imports
+import logging
+import requests
+from generic_api import GenericAPI
+
+logger = logging.getLogger(__name__)
+
+class ErgastAPI(GenericAPI):
+    """
+    Instantiates ErgastAPI object. Inherits from GenericAPI class.
+    """
+    def __init__(self, url="http://ergast.com/api/f1"):
+        """
+        Calls parent class ErgastAPI object contsructor to set base URL
+        for object.
+        """
+        super().__init__(url)
+        
+        
+    def get_historical_results(self, year_start: int, year_end: int):
+        """
+        Gets historical results between a given start year and a given 
+        end year (inclusive).
+        
+        Args:
+            year_start: Integer start year. Must be no earlier than 1950.
+            year_end: Integer end year.
+            
+        Returns:
+            List of JSON objects.
+        """
+        if year_start > year_end:
+            logger.error(f"Year range error occurred: {year_start} is 
+                         later than {year_end}. Did you mean to reverse
+                         the year arguments?")
+        results = []
+        try:
+            for year in range(year_start, year_end+1):
+            super().get()
+        except 
---
+++ generic_api.py
@@ -0,0 +1,47 @@
+import logging
+import requests
+
+logger = logging.getLogger(__name__)
+
+class GenericAPI():
+    """
+    Creates generic API object. Acts as a base class for all API classes
+    that inherit this class.
+    """
+    def __init__(self, url: str):
+        """
+        Instantiates GenericAPI object. Sets base URL for object.
+        
+        Args:
+            url: The base URL for an API request.
+            
+        Returns:
+            None
+        """
+        self.base_url = url
+        
+    def get(self, path_parameter: str):
+        """
+        Appends argument path to base URL and performs get request.
+        
+        Args:
+            path_parameter: Path parameter string that appends to base 
+            URL path before request action is sent. 
+                Example: "current/last/results"
+        
+        Returns:
+            Requested data as JSON object.
+        """
+        url = f"{self.base_url}/{path_parameter}"
+        response = requests.get(url)
+        logger.info(f"API get request sent for path URL: {url}.")
+        try:
+            # Check request status code
+            response.raise_for_status()
+            # 
+        except requests.exceptions.HTTPError as e:
+            logger.exception(f"HTTP Error occurred: {str(e)}")
+            return f"Error: {str(e)}"
+        else:
+            logger.info("Successfully returning JSON object.")
+            return response.json()
