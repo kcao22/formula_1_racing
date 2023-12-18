98dfed063b7fec33ef933d062f9d826138d044fe
kcao22 <kevincao222@gmail.com>
2023-12-18 09:34:42 -0800

    setting up terraform

---
+++ terraform/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.85.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  client_id = var.client_id
+  client_secret = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id = var.tenant_id
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoracc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+resource "azurerm_mssql_server" "example" {
+  name                         = "example-sqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_database" "example" {
+  name           = "example-db"
+  server_id      = azurerm_mssql_server.example.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = true
+  enclave_type   = "VBS"
+
+  tags = {
+    foo = "bar"
+  }
+}
+
+resource "azurerm_mssql_firewall_rule" "example" {
+  name             = "FirewallRule1"
+  server_id        = azurerm_mssql_server.example.id
+  start_ip_address = var.ip_address
+  end_ip_address   = var.ip_address
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,49 @@
+variable "client_id" {
+    type = string
+    sensitive = true
+}
+
+variable "client_secret" {
+    type = string
+    sensitive = true
+}
+
+variable "subscription_id" {
+    type = string
+    sensitive = true
+}
+
+variable "tenant_id" {
+    type = string
+    sensitive = true
+}
+
+variable "location" {
+    type = string
+    sensitive = true
+}
+
+variable "sql_server_login" {
+    type = string
+    sensitive = true
+}
+
+variable "sql_server_password" {
+    type = string
+    sensitive = true
+}
+
+variable "ip_address" {
+    type = string
+    sensitive = true
+}
+
+variable "" {
+    type = string
+    sensitive = true
+}
+
+variable "" {
+    type = string
+    sensitive = true
+}
